6e064ec7f62f71ef00641d8073970cc97ba42b11
Hineill <hineill.cespedes@pucp.edu.pe>
2024-05-09 09:25:36 -0500

    FIRST COMMIT

---
+++ src/main/java/com/example/practicando_lab6/PracticandoLab6Application.java
@@ -0,0 +1,13 @@
+package com.example.practicando_lab6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticandoLab6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticandoLab6Application.class, args);
+    }
+
+}
